6bc02509907cffebe54dff8be76620f9c56c7be3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-07-30 15:01:15 +0300

    Setup of login page view

--- theClub/src/app/home/home.component.ts
+++ theClub/src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private apiService:ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getBooks().subscribe(books => {console.log(books)})
+  }
 }
