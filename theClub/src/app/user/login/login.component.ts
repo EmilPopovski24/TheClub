5adc746b3a20ba7abaf51b76da6d1c4b351a2ea7
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-07-31 19:03:05 +0300

    setup of login form auth

--- theClub/src/app/user/login/login.component.ts
+++ theClub/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+constructor(private userService:UserService) {
+    
+}
 
+    login(email: string, password: string) :void {
+        this.userService.login();
+    }
 }
---
+++ theClub/src/app/user/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+interface User {
+  email: string;
+  firstname: string;
+//   phoneNumber: string;
+//   password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  user: User | undefined;
+    userKey = '[user]'
+
+  get isLogged():boolean {
+    return !!this.isLogged
+  }
+
+  constructor() {
+    try {
+        const lsUser = localStorage.getItem(this.userKey) || "";
+        this.user = JSON.parse(lsUser)
+    } catch(error) {
+        this.user = undefined;
+    }
+  }
+
+  login(): void {
+    this.user = {
+        email: "john@abv.bg",
+        firstname: "John",
+    };
+
+    localStorage.setItem(this.userKey, JSON.stringify(this.user))
+  }
+
+  logout(): void {
+    this.user = undefined;
+    localStorage.removeItem(this.userKey)
+  }
+}
