42187fd7be5c8ae4238017dea992293ad2fe869d
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2023-08-19 10:21:22 +0300

    Commit validations

---
+++ theClub/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { AppEmailDirective } from "./validators/app-email.directive";
+import { CommonModule } from "@angular/common"
+
+
+@NgModule({
+    declarations: [AppEmailDirective],
+    imports: [CommonModule],
+    exports: [AppEmailDirective]
+})
+
+export class SharedModule {}
+
+//export const EMAIL_DOMAINS = ["bg", "com", "net", "org"];
