2087436d0e11054e8f8c68ad45109ecfa6c4d29e
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-09 20:07:59 +0300

    Update environment.ts

--- theClub/src/environments/environment.ts
+++ theClub/src/environments/environment.ts
@@ -11,13 +11,9 @@ export const environment = {
   //   authDomain: 'theclub-db.firebaseapp.com',
   //   messagingSenderId: '22141649058',
   , booksUrl:'https://theclub-db-default-rtdb.firebaseio.com/books'
-    
-    
 };
-
 
 //apiUrl: 'https://users-49514-default-rtdb.firebaseio.com/',
 // booksUrl:'https://books-9e87c-default-rtdb.firebaseio.com'
-
 //https://books-9e87c-default-rtdb.firebaseio.com/users
 
