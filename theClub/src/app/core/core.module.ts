12fda8efdcfdbae0781129c1e6e8562974b93c76
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-05-11 10:38:35 +0300

    Update core.module.ts

--- theClub/src/app/core/core.module.ts
+++ theClub/src/app/core/core.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
-
-
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, ErrorComponent],
